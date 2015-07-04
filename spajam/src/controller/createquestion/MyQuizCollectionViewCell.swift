68f5d72bdfcbd3cddc1e23f3103805c97058dd07
Naodroid <naodroid@gmail.com>
2015-07-05 04:30:33 +0900

    カテゴリ選択画面のデザイン調整

--- spajam/src/controller/createquestion/MyQuizCollectionViewCell.swift
+++ spajam/src/controller/createquestion/MyQuizCollectionViewCell.swift
@@ -14,6 +14,7 @@ class MyQuizCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var categoryImageView: UIImageView!
     
+    @IBOutlet weak var rootView: UIView!
     @IBOutlet weak var hexImageView: UIImageView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var hasQuizImageView: UIImageView!
@@ -36,6 +37,27 @@ class MyQuizCollectionViewCell: UICollectionViewCell {
         
         let url = NSURL(string : category.imageUrl)
         self.categoryImageView.sd_setImageWithURL(url!)
+        
+        
+        let pw = self.bounds.size.width
+        let ph = self.bounds.size.height
+        
+        
+        
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let rw = CGFloat(130)
+        let rh = CGFloat(200)
+        let pw = self.bounds.size.width
+        let ph = self.bounds.size.height
+        self.rootView.bounds = CGRectMake(0, 0, rw, rh);//(pw - rw) / 2, (ph - rh) / 2, rw, rh)
+        self.rootView.center = CGPointMake(pw / 2, ph / 2)
+        
+        
+        let scale = min(pw / rw, ph / rh)
+        self.rootView.transform = CGAffineTransformMakeScale(scale, scale)
+        
     }
 }
 
